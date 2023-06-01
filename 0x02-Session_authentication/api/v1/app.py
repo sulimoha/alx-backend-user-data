d3df22555f8c35b4c08409bb47b826204a51fbe0
Mohamed Suliman <sulimmoh@gmail.com>
2023-06-01 17:39:26 -0400

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -62,8 +62,9 @@ def before_request() -> str:
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None and auth.session_coo
-    kie(request) is None: abort(401)
+    if auth.authorization_header(request) is None and auth.session_cookie(
+     request) is None:
+        abort(401)
 
     if auth.current_user(request) is None:
         abort(403)
