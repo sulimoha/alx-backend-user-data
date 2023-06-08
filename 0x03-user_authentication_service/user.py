348ead467b3c4637bbc1a93256ab16ab22837159
Mohamed Suliman <sulimmoh@gmail.com>
2023-06-08 19:19:04 -0400

    User model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,60 +1,16 @@
 #!/usr/bin/env python3
-"""DB module
-"""
-from sqlalchemy import create_engine
+"""User model"""
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
-from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.exc import InvalidRequestError
-from user import Base, User
-from typing import Type
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
 
 
-class DB:
-    """DB class
-    """
-
-    def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
-        self._engine = create_engine("sqlite:///a.db", echo=False)
-        Base.metadata.drop_all(self._engine)
-        Base.metadata.create_all(self._engine)
-        self.__session = None
-
-    @property
-    def _session(self) -> Session:
-        """Memorized session object
-        """
-        if self.__session is None:
-            DBSession = sessionmaker(bind=self._engine)
-            self.__session = DBSession()
-        return self.__session
-
-    def add_user(self, email: str, hashed_password: str) -> User:
-        """ Create a User
-        :param email: the user email (requirement)
-        :param hashed_password: the user password (requirement)
-        :return: A User Object
-        """
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
-
-        return user
-
-    def find_user_by(self, **kwargs) -> Type[User]:
-        """ Find a User by its ID
-        :param kwargs: arbitrary keyword arguments
-        :return: The first row found in the users table
-        """
-        try:
-            user = self._session.query(User).filter_by(**kwargs).first()
-        except InvalidRequestError:
-            raise InvalidRequestError
-
-        if user is None:
-            raise NoResultFound
-
-        return user
+class User(Base):
+    """User class"""
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
