08d35c64f882f1e3fd5a4a1c98b3f68a9c57cc57
Mohamed Suliman <sulimmoh@gmail.com>
2023-06-08 20:16:12 -0400

    Update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,7 +5,6 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
-from typing import Type
 from uuid import uuid4
 
 
@@ -38,9 +37,8 @@ class Auth:
         """ Register a user
         """
         try:
-            user: Type[User] = self._db.find_user_by(email=email)
-            if user is not None:
-                raise ValueError("User {} already exists".format(user.email))
+            user: User = self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists".format(user.email))
         except NoResultFound:
             decode_password: str = _hash_password(password).decode()
             new_user: User = self._db.add_user(email, decode_password)
@@ -50,38 +48,35 @@ class Auth:
         """ Check password
         """
         try:
-            user: Type[User] = self._db.find_user_by(email=email)
+            user: User = self._db.find_user_by(email=email)
+            pwd: bytes = password.encode('utf-8')
+            db_pwd: bytes = user.hashed_password.encode('utf-8')
+            return bcrypt.checkpw(pwd, db_pwd)
         except NoResultFound:
             return False
-
-        pwd: bytes = password.encode('utf-8')
-        db_pwd: bytes = user.hashed_password.encode('utf-8')
-        return bcrypt.checkpw(pwd, db_pwd)
 
     def create_session(self, email: str) -> str:
         """ Create session
         """
         try:
-            user: Type[User] = self._db.find_user_by(email=email)
+            user: User = self._db.find_user_by(email=email)
+            session_id: str = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
         except NoResultFound:
             return None
 
-        session_id: str = _generate_uuid()
-        self._db.update_user(user.id, session_id=session_id)
-        return session_id
-
-    def get_user_from_session_id(self, session_id: str) -> Type[User]:
+    def get_user_from_session_id(self, session_id: str) -> User:
         """ Find user thanks its session_id
         """
         if session_id is None:
             return None
 
         try:
-            user: Type[User] = self._db.find_user_by(session_id=session_id)
+            user: User = self._db.find_user_by(session_id=session_id)
+            return user
         except NoResultFound:
             return None
-
-        return user
 
     def destroy_session(self, user_id: int) -> None:
         """ Destroy session
@@ -90,11 +85,10 @@ class Auth:
             return None
 
         try:
-            user: Type[User] = self._db.find_user_by(id=user_id)
+            user: User = self._db.find_user_by(id=user_id)
+            self._db.update_user(user.id, session_id=None)
         except NoResultFound:
             return None
-
-        self._db.update_user(user.id, session_id=None)
 
     def get_reset_password_token(self, email: str) -> str:
         """ Generate reset password token
@@ -103,9 +97,21 @@ class Auth:
             return None
 
         try:
-            user: Type[User] = self._db.find_user_by(email=email)
+            user: User = self._db.find_user_by(email=email)
             token: str = _generate_uuid()
-            self._db.update_user(user.id, token=token)
+            self._db.update_user(user.id, reset_token=token)
             return token
+        except Exception:
+            raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ Update password
+        """
+        try:
+            user: User = self._db.find_user_by(reset_token=reset_token)
+            hash_pwd: str = _hash_password(password).decode()
+            self._db.update_user(user.id,
+                                 hashed_password=hash_pwd,
+                                 reset_token=None)
         except Exception:
             raise ValueError
